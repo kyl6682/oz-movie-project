0a06f3037c61215b67541478ffb975eb64ea5152
yurim <kyl6682@gmail.com>
2025-03-07 16:51:04 +0900

    🔊 Mission 1

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,32 +1,35 @@
 import movieListData from "../data/movieListData.json";
-import styled from "styled-components"
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-const MovieCards = styled.ul `
-    display: flex;
-    justify-content: start;
-    align-items: center;
-    gap: 20px;
-    flex-wrap: wrap;
-`
+const MovieCards = styled.ul`
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  gap: 20px;
+  flex-wrap: wrap;
+`;
 
-const MovieCard = styled.li `
-    width: 250px;
-    list-style: none;
-    img {
-        width: 200px;
-        height: 300px;
-    }
-`
+const MovieCard = styled.li`
+  width: 250px;
+  list-style: none;
+  img {
+    width: 200px;
+    height: 300px;
+  }
+`;
 
 const Movie = ({ movie }) => {
   return (
     <MovieCard key={movie.id}>
-      <img
-        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-        alt={movie.title}
-      />
-      <h3>{movie.title}</h3>
-      <div>평점: {movie.vote_average}</div>
+      <Link to={`/detail/${movie.id}`}>
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+        />
+        <h3>{movie.title}</h3>
+        <div>평점: {movie.vote_average}</div>
+      </Link>
     </MovieCard>
   );
 };
