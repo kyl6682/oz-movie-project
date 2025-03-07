fea7fcdf8de56ee676ea57b59b7082606c4b7378
yurim <kyl6682@gmail.com>
2025-03-07 12:38:21 +0900

    💄 MovieList UI 개선

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,15 +1,33 @@
 import movieListData from "../data/movieListData.json";
+import styled from "styled-components"
+
+const MovieCards = styled.ul `
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    gap: 20px;
+    flex-wrap: wrap;
+`
+
+const MovieCard = styled.li `
+    width: 250px;
+    list-style: none;
+    img {
+        width: 200px;
+        height: 300px;
+    }
+`
 
 const Movie = ({ movie }) => {
   return (
-    <li key={movie.id}>
+    <MovieCard key={movie.id}>
       <img
         src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
         alt={movie.title}
       />
       <h3>{movie.title}</h3>
       <div>평점: {movie.vote_average}</div>
-    </li>
+    </MovieCard>
   );
 };
 
@@ -17,11 +35,11 @@ function MovieList() {
   const movies = movieListData.results;
 
   return (
-    <ul>
+    <MovieCards>
       {movies.map((movie) => (
         <Movie key={movie.id} movie={movie} />
       ))}
-    </ul>
+    </MovieCards>
   );
 }
 
