59960a29288de592245697e045b7c7230de8042a
yurim <kyl6682@gmail.com>
2025-03-07 15:42:11 +0900

    💄 Slide UI 개선 및 자동 슬라이드 기능 추가

--- src/components/MovieSlide.jsx
+++ src/components/MovieSlide.jsx
@@ -6,12 +6,42 @@ import movieListData from "../data/movieListData.json";
 import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/pagination";
+import "swiper/css/autoplay";
 
 // import required modules
-import { FreeMode, Pagination } from "swiper/modules";
+import { FreeMode, Pagination, Autoplay } from "swiper/modules";
+import styled from "styled-components";
+
+const SlideDiv = styled.div`
+  position: relative;
+  &::after {
+    width: 100%;
+    height: 100%;
+    content: "";
+    position: absolute;
+    inset: 0;
+
+    background: linear-gradient(to top, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0));
+    z-index: 1;
+  }
+  img {
+    width: 100%;
+    display: block;
+  }
+  div {
+    z-index: 2;
+    position: absolute;
+    bottom: 20px;
+    left: 100px;
+    h2 {
+      color: white;
+      font-size: 68px;
+    }
+  }
+`;
 
 function MovieSlide() {
-  const MovieData = movieListData.results;
+  const MovieData = movieListData.results.slice(0, 8);
   return (
     <>
       <Swiper
@@ -22,18 +52,23 @@ function MovieSlide() {
           clickable: true,
         }}
         autoplay={{
-            delay: 3000,
-            disableOnInteraction : false
+          delay: 3000,
+          disableOnInteraction: false,
         }}
-        modules={[FreeMode, Pagination]}
+        modules={[FreeMode, Pagination, Autoplay]}
         className="mySwiper"
       >
         {MovieData.map((movie) => {
           return (
             <SwiperSlide key={movie.id}>
-              <img
-                src={`https://image.tmdb.org/t/p/w1280/${movie.backdrop_path}`}
-              />
+              <SlideDiv>
+                <img
+                  src={`https://image.tmdb.org/t/p/w1280/${movie.backdrop_path}`}
+                />
+                <div>
+                  <h2>{movie.title}</h2>
+                </div>
+              </SlideDiv>
             </SwiperSlide>
           );
         })}
