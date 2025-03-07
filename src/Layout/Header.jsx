233366ea6bc9ec24eaab49b499eeaea89da29126
yurim <kyl6682@gmail.com>
2025-03-07 14:29:37 +0900

    💄 Header UI 개선

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,14 +1,43 @@
+import styled from "styled-components"
+
+const NavStyle = styled.div`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    gap: 20px;
+`
+
+const LogoStyle = styled.div`
+    font-size: 2rem;
+    font-weight: 700;
+`
+
+const SearchInput = styled.input`
+    border: none;
+    background-color: #efefef;
+    padding: 0 16px;
+    width: 250px;
+    height: 30px;
+    border-radius: 7px;
+`
+
+const AccountDiv = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 20px;
+`
+
 function Header () {
     return (
         <>
-            <div>
-                <div>logo</div>
-                <input />
-                <div>
+            <NavStyle>
+                <LogoStyle>OZ 무비</LogoStyle>
+                <SearchInput />
+                <AccountDiv>
                     <button>로그인</button>
                     <button>회원가입</button>
-                </div>
-            </div>
+                </AccountDiv>
+            </NavStyle>
         </>
     )
 }
