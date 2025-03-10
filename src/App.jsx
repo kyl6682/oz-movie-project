fcebc0bd06f444ec1caadcd679ddbfc163373e48
yurim <kyl6682@gmail.com>
2025-03-10 15:46:07 +0900

    🐛 오타 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import About from "./pages/About";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Layout from "./Layout/Layout";
 
 function App() {
